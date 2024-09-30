4b0f09e344792b51617728d61f9f68d3c8e9d8e2
ahcamachod <andrelms4@insper.edu.br>
2024-09-30 13:30:00 -0300

    Video 3.4

--- categorizador.py
+++ categorizador.py
@@ -8,31 +8,40 @@ CHAVE_API_GOOGLE = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=CHAVE_API_GOOGLE)
 MODELO_ESCOLHIDO = "gemini-1.5-flash"
 
-lista_categorias_possiveis = "Eletrônicos Verdes,Moda Sustentável,Produtos de Limpeza Ecológicos,Alimentos Orgânicos,Produtos de Higiene Sustentáveis"
+def categorizar_produto(nome_produto, lista_categorias_possiveis):
+  prompt_sistema = f"""
+          Você é um categorizador de produtos.
+          Você deve assumir as categorias presentes na lista abaixo.
 
-prompt_sistema = f"""
-        Você é um categorizador de produtos.
-        Você deve assumir as categorias presentes na lista abaixo.
+          # Lista de Categorias Válidas
+          {lista_categorias_possiveis.split(",")}
 
-        # Lista de Categorias Válidas
-        {lista_categorias_possiveis.split(",")}
+          # Formato da Saída
+          Produto: Nome do Produto
+          Categoria: apresente a categoria do produto
 
-        # Formato da Saída
-        Produto: Nome do Produto
-        Categoria: apresente a categoria do produto
+          # Exemplo de Saída
+          Produto: Escova elétrica com recarga solar
+          Categoria: Eletrônicos Verdes
 
-        # Exemplo de Saída
-        Produto: Escova elétrica com recarga solar
-        Categoria: Eletrônicos Verdes
+      """
 
-    """
+  llm = genai.GenerativeModel(
+    model_name=MODELO_ESCOLHIDO,
+    system_instruction=prompt_sistema
+  )
 
-llm = genai.GenerativeModel(
-  model_name=MODELO_ESCOLHIDO,
-  system_instruction=prompt_sistema
-)
+  resposta = llm.generate_content(nome_produto)
+  return resposta.text
 
-pergunta = "Escova de dentes de bambu"
+def main():
+  lista_categorias_possiveis = "Eletrônicos Verdes,Moda Sustentável,Produtos de Limpeza Ecológicos,Alimentos Orgânicos,Produtos de Higiene Sustentáveis"
 
-resposta = llm.generate_content(pergunta)
-print(f"A resposta é: {resposta.text}")
+  produto = input("Informe o produto que você deseja classificar: ")
+
+  while produto != "":
+    print(f"Resposta: {categorizar_produto(produto, lista_categorias_possiveis)}")
+    produto = input("Informe o produto que você deseja classificar: ")
+
+if __name__ == "__main__":
+  main()
