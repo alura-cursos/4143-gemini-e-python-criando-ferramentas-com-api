6c236038e5306f31e7a3a9d36c0fdc5fab254784
ahcamachod <andrelms4@insper.edu.br>
2024-09-30 11:55:17 -0300

    Video 3.2

--- main.py
+++ main.py
@@ -10,9 +10,18 @@ MODELO_ESCOLHIDO = "gemini-1.5-flash"
 
 prompt_sistema = "Liste apenas os nomes dos produto, e ofereça uma breve descrição."
 
+configuracao_modelo = {
+  "temperature" : 0.1,
+  "top_p" : 1.0,
+  "top_k" : 2,
+  "max_output_tokens" : 8192,
+  "response_mime_type" : "text/plain"
+}
+
 llm = genai.GenerativeModel(
   model_name=MODELO_ESCOLHIDO,
-  system_instruction=prompt_sistema
+  system_instruction=prompt_sistema,
+  generation_config=configuracao_modelo
 )
 
 pergunta = "Liste três produtos de moda sustentável para ir ao shopping."
